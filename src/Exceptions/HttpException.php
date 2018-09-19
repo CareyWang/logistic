3303250ddf2a1e5a42b6a04442e987b4e320b4d6
cwang <cwang@i9i8.com>
2018-09-19 10:19:39 +0800

    Add other exceptions

---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cwang
+ * Date: 2018/9/18
+ * Time: 14:48
+ */
+
+namespace Careywong\Logistic\Exceptions;
+
+class HttpException extends Exception
+{
+
+}
---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Careywong\Logistic\Exceptions;
+
+class InvalidArgumentException extends Exception
+{
+
+}
